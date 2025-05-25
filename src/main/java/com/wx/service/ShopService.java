5907b93b716b70cfa5ea5af6e21452e517b83457
kevin <kevin@11.com>
2025-05-25 15:28:43 +0800

    增加接口, 修复支付宝支付

--- src/main/java/com/wx/service/ShopService.java
+++ src/main/java/com/wx/service/ShopService.java
@@ -1,6 +1,7 @@
 package com.wx.service;
 
 import com.wx.common.model.ShopConfigResponse;
+import com.wx.common.model.request.BestSellingGoodsRequest;
 import com.wx.common.model.request.ShopConfigRequest;
 import com.wx.common.model.response.ShopConfigDOResponse;
 
@@ -8,4 +9,6 @@ public interface ShopService {
     ShopConfigResponse getShopConfigInfo(ShopConfigRequest request);
 
     ShopConfigDOResponse getShopInfo(ShopConfigRequest request);
+
+    Boolean updateBestSellingGoods(BestSellingGoodsRequest request);
 }
--- src/main/java/com/wx/service/impl/ShopServiceImpl.java
+++ src/main/java/com/wx/service/impl/ShopServiceImpl.java
@@ -1,19 +1,25 @@
 package com.wx.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.wx.common.model.ShopConfigResponse;
+import com.wx.common.model.request.BestSellingGoodsRequest;
 import com.wx.common.model.request.ShopConfigRequest;
 import com.wx.common.model.response.ShopConfigDOResponse;
+import com.wx.orm.entity.GoodsDO;
 import com.wx.orm.entity.RebateDO;
 import com.wx.orm.entity.ShopConfigDO;
 import com.wx.orm.entity.UserProfileDO;
 import com.wx.orm.mapper.RebateMapper;
 import com.wx.orm.mapper.ShopConfigMapper;
+import com.wx.service.GoodsService;
 import com.wx.service.ShopService;
 import com.wx.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +28,7 @@ public class ShopServiceImpl implements ShopService {
     private final ShopConfigMapper shopConfigMapper;
     private final TokenService tokenService;
     private final RebateMapper rebateMapper;
+    private final GoodsService goodsService;
 
     @Override
     public ShopConfigResponse getShopConfigInfo(ShopConfigRequest request) {
@@ -43,5 +50,33 @@ public class ShopServiceImpl implements ShopService {
         BeanUtils.copyProperties(shopConfigDO, response);
         response.setSource(source);
         return response;
+    }
+
+    @Override
+    public Boolean updateBestSellingGoods(BestSellingGoodsRequest request) {
+        String token = request.getToken();
+        UserProfileDO userByToken = tokenService.getUserByToken(token);
+        if (userByToken == null) return false;
+
+        Long[] bestSellingGoodsIds = request.getBestSellingGoodsIds();
+        // 查询出这些商品
+        List<GoodsDO> goodsDOS = goodsService.listByIds(Arrays.asList(bestSellingGoodsIds));
+        List<Map<String, String>> bestSellingGoodsList = new ArrayList<>(goodsDOS.size());
+        for (GoodsDO goodsDO : goodsDOS) {
+            Map<String, String> bestSellingGoods = new HashMap<>();
+            bestSellingGoods.put("id", goodsDO.getId().toString());
+            bestSellingGoods.put("name", goodsDO.getName());
+            bestSellingGoods.put("goodsPic", goodsDO.getGoodsPic());
+            bestSellingGoods.put("price", String.valueOf(goodsDO.getPrice()));
+            bestSellingGoods.put("sales", String.valueOf(goodsDO.getSales())); // 销量
+            bestSellingGoodsList.add(bestSellingGoods);
+        }
+
+        String bestSellingJson = JSON.toJSONString(bestSellingGoodsList);
+        ShopConfigDO shopConfigDO = new ShopConfigDO();
+        shopConfigDO.setId(request.getId());
+        shopConfigDO.setBestSellers(bestSellingJson);
+        int updated = shopConfigMapper.updateById(shopConfigDO);
+        return updated > 0;
     }
 }
